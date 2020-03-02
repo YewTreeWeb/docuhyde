c29d622cbbf6187ddc0c95fb690416b4de12d105
Mat <mathew.teague@yewtreeweb.co.uk>
2020-03-02 09:44:48 +0000

    removed not relivent files

--- website/src/js/modules/helpers.js
+++ website/src/js/modules/helpers.js
@@ -1,53 +1,53 @@
 // Avoid `console` errors in browsers that lack a console.
 (() => {
-	let method;
-	const noop = () => {};
-	const methods = [
-		'assert',
-		'clear',
-		'count',
-		'debug',
-		'dir',
-		'dirxml',
-		'error',
-		'exception',
-		'group',
-		'groupCollapsed',
-		'groupEnd',
-		'info',
-		'log',
-		'markTimeline',
-		'profile',
-		'profileEnd',
-		'table',
-		'time',
-		'timeEnd',
-		'timeline',
-		'timelineEnd',
-		'timeStamp',
-		'trace',
-		'warn'
-	];
-	let length = methods.length;
-	let console = (window.console = window.console || {});
+  let method;
+  const noop = () => {};
+  const methods = [
+    'assert',
+    'clear',
+    'count',
+    'debug',
+    'dir',
+    'dirxml',
+    'error',
+    'exception',
+    'group',
+    'groupCollapsed',
+    'groupEnd',
+    'info',
+    'log',
+    'markTimeline',
+    'profile',
+    'profileEnd',
+    'table',
+    'time',
+    'timeEnd',
+    'timeline',
+    'timelineEnd',
+    'timeStamp',
+    'trace',
+    'warn'
+  ];
+  let length = methods.length;
+  let console = (window.console = window.console || {});
 
-	while (length--) {
-		method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-		// Only stub undefined methods.
-		if (!console[method]) {
-			console[method] = noop;
-		}
-	}
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
 })();
 
 // Add tabbing indicator to window
 const handleFirstTab = (e) => {
-	if (e.keyCode === 9) {
-		// the "I am a keyboard user" key
-		document.body.classList.add('user-is-tabbing');
-		window.removeEventListener('keydown', handleFirstTab);
-	}
+  if (e.keyCode === 9) {
+    // the "I am a keyboard user" key
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
 };
 
 window.addEventListener('keydown', handleFirstTab);
@@ -56,22 +56,22 @@ window.addEventListener('keydown', handleFirstTab);
 
 const body = document.getElementsByTagName('body')[0];
 breakpoints({
-	xlarge: [ '1281px', '1680px' ],
-	large: [ '981px', '1280px' ],
-	medium: [ '737px', '980px' ],
-	small: [ '481px', '736px' ],
-	xsmall: [ null, '480px' ]
+  xlarge: ['1281px', '1680px'],
+  large: ['981px', '1280px'],
+  medium: ['737px', '980px'],
+  small: ['481px', '736px'],
+  xsmall: [null, '480px']
 });
 
 if (browser.mobile) {
-	body.classList.add('is-mobile');
+  body.classList.add('is-mobile');
 } else {
-	breakpoints.on('>medium', () => {
-		body.classList.remove('is-mobile');
-		body.classList.add('is-desktop');
-	});
-	breakpoints.on('<=medium', () => {
-		body.classList.remove('is-desktop');
-		body.classList.add('is-mobile');
-	});
+  breakpoints.on('>medium', () => {
+    body.classList.remove('is-mobile');
+    body.classList.add('is-desktop');
+  });
+  breakpoints.on('<=medium', () => {
+    body.classList.remove('is-desktop');
+    body.classList.add('is-mobile');
+  });
 }
