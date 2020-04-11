195d63b102f01cecdcf63d3a809f77057d74a85b
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-04-11 23:07:59 +0100

    added external miniser and eslint loader to webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack'
 import yargs from 'yargs'
+import TerserPlugin from 'terser-webpack-plugin'
 const prod = yargs.argv.prod
 
 module.exports = {
@@ -15,12 +16,17 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env', 'babel-preset-airbnb'],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-transform-async-to-generator'
-          ]
+          presets: ['@babel/preset-env', 'babel-preset-airbnb']
+        }
+      },
+      {
+        enforce: 'pre',
+        test: /\.jsx$|\.es6$|\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          cache: true,
+          fix: true
         }
       }
     ]
@@ -36,6 +42,40 @@ module.exports = {
   output: {
     filename: '[name].js',
     chunkFilename: '[name].bundle.js'
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    },
+    minimize: prod ? true : false,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        terserOptions: {
+          output: {
+            comments: !prod ? true : false
+          }
+        },
+        chunkFilter: chunk => {
+          // Exclude uglification for the `vendor` chunk
+          if (chunk.name === 'vendors') {
+            return false
+          }
+
+          return true
+        },
+        cache: true,
+        extractComments: false
+      })
+    ]
   },
   externals: {
     jquery: 'jQuery',
